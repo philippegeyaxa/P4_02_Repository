a81172bd3ff03469dd4e2ba47819d622bc2c6352
philippegeyaxa <philippe.gey@axa.fr>
2020-03-11 08:58:04 +0100

    Unit tests implemented (2 failures 0 errors)

--- src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -83,7 +83,7 @@ public class FareCalculatorServiceTest {
     }
 
     @Test
-    public void calculateFareBikeWithLessThanOneHourParkingTime(){
+    public void givenBike_whenParkingTime45Minutes_fareIsCorrect(){
         Date inTime = new Date();
         inTime.setTime( System.currentTimeMillis() - (  45 * 60 * 1000) );//45 minutes parking time should give 3/4th parking fare
         Date outTime = new Date();
@@ -97,7 +97,7 @@ public class FareCalculatorServiceTest {
     }
 
     @Test
-    public void calculateFareCarWithLessThanOneHourParkingTime(){
+    public void givenCar_whenParkingTime45Minutes_fareIsCorrect(){
         Date inTime = new Date();
         inTime.setTime( System.currentTimeMillis() - (  45 * 60 * 1000) );//45 minutes parking time should give 3/4th parking fare
         Date outTime = new Date();
@@ -122,6 +122,38 @@ public class FareCalculatorServiceTest {
         ticket.setParkingSpot(parkingSpot);
         fareCalculatorService.calculateFare(ticket);
         assertEquals( (24 * Fare.CAR_RATE_PER_HOUR) , ticket.getPrice());
+    }
+
+    @Test
+    public void givenBike_whenParkingTime15Minutes_fareIsFree(){
+    	// GIVEN
+        Date inTime = new Date();
+        inTime.setTime( System.currentTimeMillis() - (  15 * 60 * 1000) );
+        Date outTime = new Date();
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.BIKE,false);
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+        // WHRN
+        fareCalculatorService.calculateFare(ticket);
+        // THEN
+        assertEquals(0, ticket.getPrice());
+    }
+
+    @Test
+    public void givenCar_whenParkingTime15Minutes_fareIsFree(){
+    	// GIVEN
+        Date inTime = new Date();
+        inTime.setTime( System.currentTimeMillis() - (  15 * 60 * 1000) );
+        Date outTime = new Date();
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+        // WHEN
+        fareCalculatorService.calculateFare(ticket);
+        // THEN
+        assertEquals(0, ticket.getPrice());
     }
 
 }
