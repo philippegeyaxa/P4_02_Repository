9fed6629d955e75e196a42abcbb9017e8e6f5d80
philippegeyaxa <philippe.gey@axa.fr>
2020-03-12 13:31:14 +0100

    Encoding defined

--- src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
+++ src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class InputReaderUtil {
 
-    private static Scanner scan = new Scanner(System.in);
+    private static Scanner scan = new Scanner(System.in, "ISO-8859-1");
     private static final Logger logger = LogManager.getLogger("InputReaderUtil");
 
     public int readSelection() {
